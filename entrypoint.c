d41ca85629157c5bd2087b5667e923e23a0530c4
ne1mnn <sabitovkirill@ya.ru>
2024-01-29 19:57:59 +0000

    Initial Commit

---
+++ entrypoint.c
@@ -0,0 +1,21 @@
+//
+// Created by ne1mnn on 1/29/24.
+//
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Ivanov Ivan");
+MODULE_VERSION("0.01");
+
+int dmp_init(void) {
+    printk(KERN_INFO "Hello, World!\n");
+    return 0;
+}
+
+void dmp_exit(void) { printk(KERN_INFO "Goodbye!\n"); }
+
+module_init(dmp_init);
+module_exit(dmp_exit);
